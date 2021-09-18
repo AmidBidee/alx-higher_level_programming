ae938f01f2c99d35ec32e0c9051791b2e7da299b
krummitz <amidbidee@gmail.com>
2021-09-18 02:06:26 +0100

    fixes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    m_copy = matrix.copy()
-    m_copy = [[(i[n]**2) for n in range(3)] for i in m_copy]
+    m_copy = [[(i[n]**2) for n in range(3)] for i in matrix]
     return m_copy
