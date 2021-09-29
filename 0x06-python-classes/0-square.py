cc34cefa8e4573e72784f850e454427b4637c29b
krummitz <amidbidee@gmail.com>
2021-09-29 01:54:48 +0100

    fixes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,7 +3,6 @@
 This is the "Square"  module.
 This module provides a simple Square class.
 """
-
 class Square:
     """A simple Square class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,10 +3,7 @@
 This is the "Square"  module.
 This module provides a simple Square class with initialize size.
 """
-
-
 class Square:
     """A class that defines a Square by size"""
     def __init__(self, size):
         self.__size = size
-
