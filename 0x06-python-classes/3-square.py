3fdf3c1a54973e4be9692483fc43df21662385ab
krummitz <amidbidee@gmail.com>
2021-09-29 02:24:16 +0100

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A simple square module
+"""
+
+
+class Square:
+    """ A simpe square class """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        a = self.__size ** 2
+        return a
