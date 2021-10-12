195fe94778b5cc81e1fba6464ee929fd1fcd67f8
krummitz <amidbidee@gmail.com>
2021-10-12 07:48:16 +0100

    fixes

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" This module holds MyList object """
+
+
 class MyList(list):
+    """ MyList object class """
     def print_sorted(self):
+        """ A function that sorts and prints the inherited list """
         print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+""" This mpdule includes the is_same_class object class """
+
+
 def is_same_class(obj, a_class):
+    """ 
+    returns True if the object is exactly an instance 
+    of the specified class ; otherwise False
+    """
     return type(obj) == a_class
