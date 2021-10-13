619e8d7645e2fc03a398c25e964f4ce35df4473d
krummitz <amidbidee@gmail.com>
2021-10-13 09:09:23 +0100

    fixes

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,21 +3,24 @@
 student module
 
 Student(first_name, last_name): Student object class
-[class].to_json(): returns dictionary represention of student instance
+[class].to_json(attrs=None): returns dictionary represention of student instance
 """
 
 
 class Student:
+    """ Student object class """
     first_name = ""
     last_name = ""
     age = ""
 
     def __init__(self, first_name, last_name, age):
+        """ Student instance class constructor """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """ Returns dictionary represention of student instance """
         _dict = {}
         if attrs:
             for attr in attrs:
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,14 +8,17 @@ Student(first_name, last_name): Student object class
 
 
 class Student:
+    """ Student object class """
     first_name = ""
     last_name = ""
     age = ""
 
     def __init__(self, first_name, last_name, age):
+        """ object instance constructor """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ returns dictionary represention of student instance """
         return self.__dict__
