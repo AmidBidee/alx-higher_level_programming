daa78e681a93ebb8568595217d947d3632679c42
krummitz <amidbidee@gmail.com>
2021-12-15 02:09:05 +0100

    initial commits + advanced tasks

--- 0x12-javascript-warm_up/tutor.js
+++ 0x12-javascript-warm_up/tutor.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+
+// process.argv
+const { argv } = require('process');
+
+
+argv.forEach((val, index) => {
+	console.log(`${index}: ${val}`);
+});
+
+let len = 0;
+argv.forEach((val, index) => {
+	len++;
+});
+
+if (len > 2) {
+	if (len == 3) {
+		console.log('Argument found');
+	}
+	else if (len > 3) {
+		console.log('Arguments found');
+	}
+else {
+	console.log('No Argument');
+}
+}
+
