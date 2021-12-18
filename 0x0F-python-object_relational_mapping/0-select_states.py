4a2871250a0078583f6905d73da0063f58ec5721
krummitz <amidbidee@gmail.com>
2021-12-18 07:37:28 +0100

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+0-select_states module
+"""
+
+
+def select_states(u: str, passwd: str, name: str):
+    """
+    prints list of all states in a database
+
+    Args:
+        [u:str]: username to connect as
+        [passwd:str]: users password
+        [name:str]: database name to connect to
+
+    Return:
+        None
+    """
+
+    # setup my connection and query
+    host = "localhost"
+    port = 3306
+    query_string = """SELECT * FROM states ORDER BY id ASC"""
+    db = MySQLdb.connect(host, u, passwd, name)
+
+    # get cursor and execute query
+    cur = db.cursor()
+    cur.execute(query_string)
+    result = cur.fetchall()
+
+    # print results in human readable form
+    for row in result:
+        print(row, end='\n')
+
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    lt = len(sys.argv)
+    if lt >= 2:
+        u, p, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    select_states(u, p, db_name)
