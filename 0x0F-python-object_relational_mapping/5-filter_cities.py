50df85a2c6adabafd6186219a04a34f63f2c366b
krummitz <amidbidee@gmail.com>
2021-12-20 00:12:33 +0100

    fixes

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all cities from the database
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    host = 'localhost'
+    port = 3306
+    u, passwd, db_name, search = (sys.argv[1],
+                                  sys.argv[2],
+                                  sys.argv[3],
+                                  sys.argv[4])
+
+    # connect to db
+    db = MySQLdb.connect(host, u, passwd, db_name, port=port)
+    cur = db.cursor()
+
+    # query db
+    query_string = """SELECT cities.id, cities.name, states.name
+                        FROM states
+                        INNER JOIN cities ON states.id = cities.state_id
+                        WHERE states.name = '{}'
+                        ORDER BY cities.id ASC""".format(search)
+    cur.execute(query_string)
+
+    # print result
+    results = cur.fetchall()
+    for row in results:
+        print(row, end='\n')
+
+    # close db and connection
+    cur.close()
+    db.close()
