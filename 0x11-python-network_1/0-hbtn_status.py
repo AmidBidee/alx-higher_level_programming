d7cb2bd814fdc10c3f16da71f7111e8808745173
krummitz <amidbidee@gmail.com>
2022-01-10 00:16:46 +0100

    freaking semicolon in string

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
     with request.urlopen(url) as response:
         res = response.read()
-        print('Body response')
+        print('Body response:')
         print('\t- {}'.format(type(res)))
         print('\t- {}'.format(res))
         print('\t- {}'.format(res.decode('utf-8')))
