1c4c1ada30969b2a4151dc8dad62bc2f76811257
krummitz <amidbidee@gmail.com>
2022-01-10 00:23:27 +0100

    shaking my head

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,10 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 
-
-if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        res = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(res)))
-        print('\t- content: {}'.format(res))
-        print('\t- utf8 content: {}'.format(res.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
