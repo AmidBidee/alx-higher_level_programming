ea6131b0a6b5f2141410cccd066b4686627e955d
krummitz <amidbidee@gmail.com>
2022-01-10 00:21:28 +0100

    sigh!

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
-import urllib.request as request
+import urllib.request
 
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
-    with request.urlopen(url) as response:
+    with urllib.request.urlopen(url) as response:
         res = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(res)))
